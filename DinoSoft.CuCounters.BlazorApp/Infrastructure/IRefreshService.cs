90c1e581f8992c6781a99c432fce47c9a18dec09
Eliseev Eugene <eliseevmail@ya.ru>
2022-02-04 01:07:16 +0300

    RefreshToken

--- DinoSoft.CuCounters.BlazorApp/Infrastructure/IRefreshService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DinoSoft.CuCounters.BlazorApp.Infrastructure
-{
-    internal interface IRefreshService
-    {
-        void Add(Type key, Action action);
-
-        void RequestRefresh(Type key);
-    }
-}
---
+++ DinoSoft.CuCounters.BlazorApp/Infrastructure/RefreshToken.cs
@@ -0,0 +1,31 @@
+﻿namespace DinoSoft.CuCounters.BlazorApp.Infrastructure
+{
+    public class RefreshToken
+    {
+        private List<Action> refreshActions;
+
+        public RefreshToken(List<Action> refreshActions = null)
+        {
+            if (refreshActions != null)
+            {
+                this.refreshActions = refreshActions.ToList();
+            }
+            else
+            {
+                this.refreshActions = new List<Action>();
+            }
+        }
+
+        public RefreshToken Next() => new RefreshToken(refreshActions);
+
+        public void Add(Action refreshAction)
+        {
+            refreshActions.Add(refreshAction);
+        }
+
+        public void Refresh()
+        {
+            refreshActions.ForEach(x => x.Invoke());
+        }
+    }
+}
