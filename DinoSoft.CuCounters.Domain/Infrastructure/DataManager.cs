79e6514e165db97831df6cc3d4e3226a3b0b421c
Eliseev Eugene <eliseevmail@ya.ru>
2022-02-06 22:09:27 +0300

    create text data

--- DinoSoft.CuCounters.Domain/Infrastructure/DataManager.cs
+++ DinoSoft.CuCounters.Domain/Infrastructure/DataManager.cs
@@ -15,7 +15,8 @@ namespace DinoSoft.CuCounters.Domain.Infrastructure
 
         public IEnumerable<CounterGroup> GetCounterGroups()
         {
-            dataContext = dataContextRepository.DataContext ;
+            //dataContext = GenerateTestDataContext();
+            dataContext = dataContextRepository.DataContext;
             return dataContext.CounterGroups.Select(x => new CounterGroup(x));
         }
 
@@ -25,6 +26,74 @@ namespace DinoSoft.CuCounters.Domain.Infrastructure
             {
                 dataContextRepository.Save(dataContext);
             }
+        }
+
+        private Data.Model.DataContext GenerateTestDataContext()
+        {
+            var dataContext = new Data.Model.DataContext()
+            {
+                CounterGroups = new List<Data.Model.CounterGroup>()
+            };
+
+            for (int i = 0; i < 10; i++)
+            {
+                dataContext.CounterGroups.Add(
+                    new Data.Model.CounterGroup
+                    {
+                        Name = $"Карма",
+                        Id = Guid.NewGuid(),
+                        IconName = "bi-diamond",
+                        Counters = new List<Data.Model.Counter>()
+                        {
+                            GenCounter(),
+                            GenCounter(),
+                            GenCounter(),
+                            GenCounter(),
+                            GenCounter(),
+                            GenCounter(),
+                            GenCounter(),
+                            GenCounter(),
+                            GenCounter(),
+                            GenCounter(),
+                            GenCounter(),
+                            GenCounter(),
+                            GenCounter(),
+                            GenCounter(),
+                        }
+                    });
+            }
+
+            Data.Model.Counter GenCounter() => new Data.Model.Counter()
+            {
+                Id = Guid.NewGuid(),
+                Value = 1,
+                Name = $"Карма",
+                IconName = "bi-diamond",
+                SortOrder = 0,
+                CounterActions = new List<Data.Model.CounterAction>
+                {
+                    new Data.Model.CounterAction()
+                    {
+                        Id = Guid.NewGuid(),
+                        Value = 1,
+                        ActionType = Data.Model.CounterActionType.Add
+                    },
+                    new Data.Model.CounterAction()
+                    {
+                        Id = Guid.NewGuid(),
+                        Value = 5,
+                        ActionType = Data.Model.CounterActionType.Add
+                    },
+                    new Data.Model.CounterAction()
+                    {
+                        Id = Guid.NewGuid(),
+                        Value = 1,
+                        ActionType = Data.Model.CounterActionType.Sub
+                    }
+                }
+            };
+
+            return dataContext;
         }
     }
 }
