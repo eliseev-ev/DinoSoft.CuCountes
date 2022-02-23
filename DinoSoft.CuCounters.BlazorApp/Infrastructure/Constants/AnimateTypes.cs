6c6b6b5a984d0213ae0587a2b73304477ffe15ab
Eliseev Eugene <eliseevmail@ya.ru>
2022-02-23 22:05:29 +0300

    Add anime

---
+++ DinoSoft.CuCounters.BlazorApp/Infrastructure/Constants/AnimateTypes.cs
@@ -0,0 +1,168 @@
+﻿namespace DinoSoft.CuCounters.BlazorApp.Infrastructure.Constants
+{
+    public static class Animates
+    {
+        public static class AttentionSeekers
+        {
+            public static string Bounce => "animate__animated animate__bounce";
+            public static string Flash => "animate__animated animate__flash";
+            public static string Pulse => "animate__animated animate__pulse";
+            public static string RubberBand => "animate__animated animate__rubberBand";
+            public static string ShakeX => "animate__animated animate__shakeX";
+            public static string ShakeY => "animate__animated animate__shakeY";
+            public static string HeadShake => "animate__animated animate__headShake";
+            public static string Wwing => "animate__animated animate__r";
+            public static string Tada => "animate__animated animate__tada";
+            public static string Wobble => "animate__animated animate__wobble";
+            public static string Jello => "animate__animated animate__jello";
+            public static string HeartBeat => "animate__animated animate__heartBeat";
+        }
+
+        public static class BackEntrances
+        {
+            public static string backInDown => "animate__animated animate__backInDown";
+            public static string backInLeft => "animate__animated animate__backInLeft";
+            public static string backInRight => "animate__animated animate__backInRight";
+            public static string backInUp => "animate__animated animate__backInUp";
+        }
+
+        public static class BackExits
+        {
+            public static string BackOutDown => "animate__animated animate__backOutDown";
+            public static string BackOutLeft => "animate__animated animate__backOutLeft";
+            public static string BackOutRight => "animate__animated animate__backOutRight";
+            public static string BackOutUp => "animate__animated animate__backOutUp";
+        }
+
+        public static class BouncingEntrances
+        {
+            public static string BounceIn => "animate__animated animate__bounceIn";
+            public static string BounceInDown => "animate__animated animate__bounceInDown";
+            public static string BounceInLeft => "animate__animated animate__bounceInLeft";
+            public static string BounceInRight => "animate__animated animate__bounceInRight";
+            public static string BounceInUp => "animate__animated animate__bounceInUp";
+        }
+
+        public static class BouncingExits
+        {
+            public static string BounceOut => "animate__animated animate__bounceOut";
+            public static string BounceOutDown => "animate__animated animate__bounceOutDown";
+            public static string BounceOutLeft => "animate__animated animate__bounceOutLeft";
+            public static string BounceOutRight => "animate__animated animate__bounceOutRight";
+            public static string BounceOutUp => "animate__animated animate__bounceOutUp";
+        }
+
+        public static class FadingEntrances
+        {
+            public static string FadeIn => "animate__animated animate__fadeIn";
+            public static string FadeInDown => "animate__animated animate__fadeInDown";
+            public static string FadeInDownBig => "animate__animated animate__fadeInDownBig";
+            public static string FadeInLeft => "animate__animated animate__fadeInLeft";
+            public static string FadeInLeftBig => "animate__animated animate__fadeInLeftBig";
+            public static string FadeInRight => "animate__animated animate__fadeInRight";
+            public static string FadeInRightBig => "animate__animated animate__fadeInRightBig";
+            public static string FadeInUp => "animate__animated animate__fadeInUp";
+            public static string FadeInUpBig => "animate__animated animate__fadeInUpBig";
+            public static string FadeInTopLeft => "animate__animated animate__fadeInTopLeft";
+            public static string FadeInTopRight => "animate__animated animate__fadeInTopRight";
+            public static string FadeInBottomLeft => "animate__animated animate__fadeInBottomLeft";
+            public static string FadeInBottomRight => "animate__animated animate__fadeInBottomRight";
+        }
+
+        public static class FadingExits
+        {
+            public static string FadeOut => "animate__animated animate__fadeOut";
+            public static string FadeOutDown => "animate__animated animate__fadeOutDown";
+            public static string FadeOutDownBig => "animate__animated animate__fadeOutDownBig";
+            public static string FadeOutLeft => "animate__animated animate__fadeOutLeft";
+            public static string FadeOutLeftBig => "animate__animated animate__fadeOutLeftBig";
+            public static string FadeOutRight => "animate__animated animate__fadeOutRight";
+            public static string FadeOutRightBig => "animate__animated animate__fadeOutRightBig";
+            public static string FadeOutUp => "animate__animated animate__fadeOutUp";
+            public static string FadeOutUpBig => "animate__animated animate__fadeOutUpBig";
+            public static string FadeOutTopLeft => "animate__animated animate__fadeOutTopLeft";
+            public static string FadeOutTopRight => "animate__animated animate__fadeOutTopRight";
+            public static string FadeOutBottomRight => "animate__animated animate__fadeOutBottomRight";
+            public static string FadeOutBottomLeft => "animate__animated animate__fadeOutBottomLeft";
+        }
+
+        public static class Flippers
+        {
+            public static string Flip => "animate__animated animate__flip";
+            public static string FlipInX => "animate__animated animate__flipInX";
+            public static string FlipInY => "animate__animated animate__flipInY";
+            public static string FlipOutX => "animate__animated animate__flipOutX";
+            public static string FlipOutY => "animate__animated animate__flipOutY";
+        }
+
+        public static class Lightspeed
+        {
+            public static string LightSpeedInRight => "animate__animated animate__lightSpeedInRight";
+            public static string LightSpeedInLeft => "animate__animated animate__lightSpeedInLeft";
+            public static string LightSpeedOutRight => "animate__animated animate__lightSpeedOutRight";
+            public static string LightSpeedOutLeft => "animate__animated animate__lightSpeedOutLeft";
+        }
+
+        public static class RotatingEntrances
+        {
+            public static string RotateIn => "animate__animated animate__rotateIn";
+            public static string RotateInDownLeft => "animate__animated animate__rotateInDownLeft";
+            public static string RotateInDownRight => "animate__animated animate__rotateInDownRight";
+            public static string RotateInUpLeft => "animate__animated animate__rotateInUpLeft";
+            public static string RotateInUpRight => "animate__animated animate__rotateInUpRight";
+        }
+
+        public static class RotatingExits
+        {
+            public static string RotateOut => "animate__animated animate__rotateOut";
+            public static string RotateOutDownLeft => "animate__animated animate__rotateOutDownLeft";
+            public static string RotateOutDownRight => "animate__animated animate__rotateOutDownRight";
+            public static string RotateOutUpLeft => "animate__animated animate__rotateOutUpLeft";
+            public static string RotateOutUpRight => "animate__animated animate__rotateOutUpRight";
+        }
+
+        public static class Specials
+        {
+            public static string Hinge => "animate__animated animate__hinge";
+            public static string JackInTheBox => "animate__animated animate__jackInTheBox";
+            public static string RollIn => "animate__animated animate__rollIn";
+            public static string RollOut => "animate__animated animate__rollOut";
+        }
+
+        public static class ZoomingEntrances
+        {
+            public static string ZoomIn => "animate__animated animate__zoomIn";
+            public static string ZoomInDown => "animate__animated animate__zoomInDown";
+            public static string ZoomInLeft => "animate__animated animate__zoomInLeft";
+            public static string ZoomInRight => "animate__animated animate__zoomInRight";
+            public static string ZoomInUp => "animate__animated animate__zoomInUp";
+        }
+
+
+        public static class ZoomingExits
+        {
+            public static string ZoomOut => "animate__animated animate__zoomOut";
+            public static string ZoomOutDown => "animate__animated animate__zoomOutDown";
+            public static string ZoomOutLeft => "animate__animated animate__zoomOutLeft";
+            public static string ZoomOutRight => "animate__animated animate__zoomOutRight";
+            public static string ZoomOutUp => "animate__animated animate__zoomOutUp";
+        }
+
+        public static class SlidingEntrances
+        {
+            public static string SlideInDown => "animate__animated animate__slideInDown";
+            public static string SlideInLeft => "animate__animated animate__slideInLeft";
+            public static string SlideInRight => "animate__animated animate__slideInRight";
+            public static string SlideInUp => "animate__animated animate__slideInUp";
+
+        }
+
+        public static class SlidingExits
+        {
+            public static string SlideOutDown => "animate__animated animate__slideOutDown";
+            public static string SlideOutLeft => "animate__animated animate__slideOutLeft";
+            public static string SlideOutRight => "animate__animated animate__slideOutRight";
+            public static string SlideOutUp => "animate__animated animate__slideOutUp";
+        }
+    }
+}
